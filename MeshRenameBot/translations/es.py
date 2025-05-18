e24b5a5f0eae5b0a24b2043df8bc6a8d508a4d1a
humtpaglu <jesuschristusa456@gmail.com>
2025-05-18 13:49:34 +0530

    Update es.py

--- MeshRenameBot/translations/es.py
+++ MeshRenameBot/translations/es.py
@@ -11,8 +11,8 @@ class SpanishTranslations:
     )
 
     START_MSG = (
-        "👋 ¡Hola! Soy **Mesh Rename Bot**.\n\n"
-        "🌟 Código Abierto: [GitHub](https://github.com/yash-dk/Mesh-RenameBot/tree/master)\n"
+        "👋 ¡Hola! Soy **MAX Rename Bot**.\n\n"
+        "🌟 Código Abierto: [TG](https://t.me/Max_Leech_Zone_Update)\n"
         "🚀 ¡Puedes desplegar tu propia instancia!"
     )
 
