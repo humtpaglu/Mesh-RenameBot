72dc0794ed7f0cce9520018a66c67091e833015e
humtpaglu <jesuschristusa456@gmail.com>
2025-05-18 13:48:11 +0530

    Update en.py

--- MeshRenameBot/translations/en.py
+++ MeshRenameBot/translations/en.py
@@ -9,9 +9,9 @@ class EnglishTranslations:
     WRONG_VALUE_ERROR = "❌ Invalid value entered for {{ variable_name }} variable."
 
     START_MSG = (
-        "👋 Hello there! I am **Mesh Rename Bot**.\n\n"
-        "🌟 Open Source: [GitHub](https://github.com/yash-dk/Mesh-RenameBot/tree/master)\n"
-        "🚀 You can deploy your own instance!"
+        "👋 Hello there! I am **Max Rename Bot**.\n\n"
+        "🌟 TG Source: [TG](https://t.me/Max_Leech_Zone_Update)\n"
+        "🚀 POWARD BY MAX LEECH ZONE!"
     )
 
     CANCEL_MESSAGE = "⚠️ The rename has been **canceled**. Will be updated soon."
